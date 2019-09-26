9fb1f4022bdf30b32b5cca2d011d65df40f26b56
bokodimate <bokodimate@gmail.com>
2019-09-26 14:54:51 +0200

    Random kép generálás

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
+var images =["Bács-Kiskun", "Baranya", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala"];
+var megnemvolt =["Bács-Kiskun", "Baranya", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala"];
+
 function rndImg(){
-    images= new Array();
-    images[0]="<img src='file:///D:/suli/lab1/webgame%201/images/gyula.jpg'";
-    images[1]="<img src='file:///D:/suli/lab1/webgame%201/images/heviz.jpg'";
-    images[2]="<img src='file:///D:/suli/lab1/webgame%201/images/sopron1.jpg'";
-    images[3]="<img src='file:///D:/suli/lab1/webgame%201/images/tihany.jpg'";
-    images[4]="<img src='file:///D:/suli/lab1/webgame%201/images/visegrad1.jpg'";
+    var number = Math.floor(Math.random() * 19);
+    var result = "images/" + images[number] + ".jpg";
+    document.getElementById("image").src = result;
+}
 
-    random=Math.floor(Math.random()*images.length);
-    document.write(images[random]);
-
-
-
-
-
+function felt() {
+	
 }
 
 rndImg();
