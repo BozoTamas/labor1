1080512c4bd740e7755d4a725c5c914ef3176391
Csorba László <csorbalaci@gmail.com>
2019-10-02 16:33:19 +0200

    Kezelőfelület letisztítása, Dokumentáció

---
+++ labor1/main.js
@@ -0,0 +1,14 @@
+var images =["Bács-Kiskun", "Baranya", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala"];
+var megnemvolt =["Bács-Kiskun", "Baranya", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala"];
+
+function rndImg(){
+    var number = Math.floor(Math.random() * 19);
+    var result = "images/" + images[number] + ".jpg";
+    document.getElementById("image").src = result;
+}
+
+function felt() {
+
+}
+
+rndImg();
